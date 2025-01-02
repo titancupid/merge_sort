cf42b6771f5e9799c1673a749590bb131fb8aa06
titancupid <141187361+titancupid@users.noreply.github.com>
2025-01-02 01:36:56 -0500

    split arrays into singular. dont know if it is working for all sub arrays or just one hald. also need to work on sort and re-merge

--- merge.cpp
+++ merge.cpp
@@ -8,6 +8,7 @@ then you combine adjacent arrays into bigger arrays and sort them
 you continue this process until the array is properly sorted*/
 
 void test_rec(int &i);
+void test_arr(int arr, int arr1[], int arr2[]);
 int merge_func(int arr[], int &size);
 
 int main()
@@ -22,44 +23,43 @@ int main()
 
 int merge_func(int arr[], int &size)
 {
-    /*1) create two arrays of half the size and repeat until 
+    /*1) create two arrays of half the size and repeat until
         there is one eliment in each array that
-      2) combine the arrays so there are two eliments in each 
-        of the arrays 
+      2) combine the arrays so there are two eliments in each
+        of the arrays
       3) sort these sub arrays then repeat step two until the
         array is fully sorted*/
     if (size % 2 == 1)
         size += 1;
 
-    int half_size = size / 2;
-    // cout << "size: " << size << endl;
-    // cout << "half size: " << half_size << endl;
-    int* arr1 = new int[half_size];
-    for(int i = 0; i < half_size; i++){
+    size /= 2;
+    cout << "\nsize: " << size << endl;
+
+    int *arr1 = new int[size];
+    for (int i = 0; i < size; i++)
+    {
         arr1[i] = arr[i];
     }
 
-    int* arr2 = new int[half_size];
-    for(int i = 0; i < half_size; i++){
-        arr2[i] = arr[half_size+i];
+    int *arr2 = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr2[i] = arr[size + i];
     }
-    
-    //test arrays to see if populated
-    for(int i = 0; i < half_size; i++){
-        cout << arr1[i] << ", ";
+
+    // test arrays to see if populated
+    test_arr(size, arr1, arr2);
+
+    if (size != 1)
+    {
+        //cout << "\nmakes here1: " << endl;
+        merge_func(arr1, size);
     }
-    delete arr1;
-
-    cout << endl;
-    cout << endl;
-
-    for(int i = 0; i < half_size; i++){
-        cout << arr2[i] << ", ";
+    if (size != 1)
+    {
+        //cout << "\nmakes here2: " << endl;
+        merge_func(arr2, size);
     }
-    delete arr2;
-
-    return 0;
-    ////////////////////////////////////
 }
 
 void test_rec(int &i)
@@ -72,4 +72,25 @@ void test_rec(int &i)
         return;
     }
     test_rec(i);
+}
+
+void test_arr(int half_size, int arr1[], int arr2[]){
+    // test arrays to see if populated
+    for (int i = 0; i < half_size; i++)
+    {
+        cout << arr1[i] << ", ";
+    }
+    //delete arr1;
+
+    cout << endl;
+    cout << endl;
+
+    for (int i = 0; i < half_size; i++)
+    {
+        cout << arr2[i] << ", ";
+    }
+    //delete arr2;
+
+    return;
+    ////////////////////////////////////
 }
