3a2d42f29c9c39d5f62e97d72d68f3a9836f6bed
titancupid <141187361+titancupid@users.noreply.github.com>
2025-01-01 23:29:09 -0500

    split array in two. now need to do until I have many singular arrays

--- merge.cpp
+++ merge.cpp
@@ -7,21 +7,59 @@ sub arrays until you only have one eliment in each sub array.
 then you combine adjacent arrays into bigger arrays and sort them
 you continue this process until the array is properly sorted*/
 
-int merge_func(vector<int> arr);
 void test_rec(int &i);
+int merge_func(int arr[], int &size);
 
 int main()
 {
+    // int num = 0;
+    // test_rec(num);
+
     int arr[] = {4, 18, 3, 94, 12, 8, 69, 14, 5, 1};
-    
-    int num = 0;
-    test_rec(num);
+    int size = sizeof(arr)/sizeof(arr[0]);
+    merge_func(arr, size); 
 }
 
-int merge_func(vector<int> arr)
+int merge_func(int arr[], int &size)
 {
-    int size = sizeof(arr);
-    int *subarr = new int[size];
+    /*1) create two arrays of half the size and repeat until 
+        there is one eliment in each array that
+      2) combine the arrays so there are two eliments in each 
+        of the arrays 
+      3) sort these sub arrays then repeat step two until the
+        array is fully sorted*/
+    if (size % 2 == 1)
+        size += 1;
+
+    int half_size = size / 2;
+    // cout << "size: " << size << endl;
+    // cout << "half size: " << half_size << endl;
+    int* arr1 = new int[half_size];
+    for(int i = 0; i < half_size; i++){
+        arr1[i] = arr[i];
+    }
+
+    int* arr2 = new int[half_size];
+    for(int i = 0; i < half_size; i++){
+        arr2[i] = arr[half_size+i];
+    }
+    
+    //test arrays to see if populated
+    for(int i = 0; i < half_size; i++){
+        cout << arr1[i] << ", ";
+    }
+    delete arr1;
+
+    cout << endl;
+    cout << endl;
+
+    for(int i = 0; i < half_size; i++){
+        cout << arr2[i] << ", ";
+    }
+    delete arr2;
+
+    return 0;
+    ////////////////////////////////////
 }
 
 void test_rec(int &i)
