e2e84afedcf8dafe051c836665351fd4157d6f0e
titancupid <141187361+titancupid@users.noreply.github.com>
2025-01-12 15:06:53 -0500

    merge sort complete. made sure to update arr1 and arr2 with output from the split function so they are single eliments and sorted and to dealocate dynamic memory

--- merge.cpp
+++ merge.cpp
@@ -10,7 +10,7 @@ you continue this process until the array is properly sorted*/
 void test_rec(int &i);
 void test_arr(int size1, int size2, int arr1[], int arr2[]);
 void test_merged(int merged[], int newsize);
-int split_func(int arr[], int &size);
+int* split_func(int arr[], int &size);
 int* merge_func(int arr1[], int arr2[], int size1, int size2);
 
 int main()
@@ -20,10 +20,15 @@ int main()
 
     int arr[] = {4, 18, 3, 94, 12, 8, 69, 14, 5, 1};
     int size = sizeof(arr)/sizeof(arr[0]);
-    split_func(arr, size); 
+    int* sorted = split_func(arr, size); 
+
+    for(int i = 0; i < size; i++){
+        cout << sorted[i] << ", ";
+    }
+    delete sorted;
 }
 
-int split_func(int arr[], int &size)
+int* split_func(int arr[], int &size)
 {
     /*1) create two arrays of half the size and repeat until
         there is one eliment in each array that
@@ -62,38 +67,74 @@ int split_func(int arr[], int &size)
     //test_arr(size1, size2, arr1, arr2);
 
     if (size1 != 1)
-        split_func(arr1, size1);
+        arr1 = split_func(arr1, size1);
 
     if (size2 != 1)
-        split_func(arr2, size2);
+        arr2 = split_func(arr2, size2);
 
-    merge_func(arr1, arr2, size1, size2); // merge call
 
+    int* sorted =  merge_func(arr1, arr2, size1, size2); // merge call
+
+    delete arr1;
+    delete arr2;
+
+    return sorted;
 }
 
-int* merge_func(int arr1[], int arr2[], int size1, int size2){
+int* merge_func(int arr1[], int arr2[], int size1, int size2)
+{
     /*1) take the two arrays input and combine them to one array
-      2) sort these arrays then repeat until one input array is 
+      2) sort these arrays then repeat until one input array is
         empty*/
-    
-    cout << size1 << " " << size2 << endl;
+
     int newsize = size1 + size2;
-    int* merged = new int[newsize];
+    int *merged = new int[newsize];
 
     /*Need to fill the new merged array with the contents of both
-        arr1 and arr2*/
-    for(int i = 0; i < size1; i++){
-        merged[i] = arr1[i];
+        arr1 and arr2 and sort as the arrays are being merged */
+
+    /*need to sort as we merge so when we are adding an eliment 
+    to the merged array we need to check if it is smaller than 
+    the eliments already in the array so we know where to 
+    insert it*/
+
+    int i = 0, j = 0, k = 0;
+    // i is the index of the merged array
+    // j is the index of the array 1
+    // k is the index of the array 2
+
+    while (j < size1 && k < size2)
+    {
+        if (arr1[j] < arr2[k])
+        {
+            merged[i] = arr1[j];
+            i++;
+            j++;
+        }
+        else
+        {
+            merged[i] = arr2[k];
+            i++;
+            k++;
+        }
     }
-        cout << endl;
 
-    for(int i = 0; i < size2; i++){
-        merged[i+size1] = arr2[i];
+    //copy remaining eliments
+    while (j < size1)
+    {
+        merged[i] = arr1[j];
+        i++;
+        j++;
     }
-    cout << endl;
 
-    test_merged(merged, newsize);
+    while (k < size2)
+    {
+        merged[i] = arr2[k];
+        i++;
+        k++;
+    }
 
+    //test_merged(merged, newsize);
     return merged;
 }
 
